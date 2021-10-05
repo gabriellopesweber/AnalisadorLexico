5b4b9dd6a3bef592456173ed3b8d61c13f048869
Gabriel Lopes Weber <gabriellw.grb@gmail.com>
2021-10-04 21:11:19 -0300

    .

--- src/principal/Token.java
+++ src/principal/Token.java
@@ -7,5 +7,37 @@ public class Token {
 	private String[] tipoDeAcao; //Valida/Atribui/Remove
 	
 	private double perna;
+
+	public double getTokenInicial() {
+		return tokenInicial;
+	}
+
+	public void setTokenInicial(double tokenInicial) {
+		this.tokenInicial = tokenInicial;
+	}
+
+	public double getTokenFinal() {
+		return tokenFinal;
+	}
+
+	public void setTokenFinal(double tokenFinal) {
+		this.tokenFinal = tokenFinal;
+	}
+
+	public String[] getTipoDeAcao() {
+		return tipoDeAcao;
+	}
+
+	public void setTipoDeAcao(String[] tipoDeAcao) {
+		this.tipoDeAcao = tipoDeAcao;
+	}
+
+	public double getPerna() {
+		return perna;
+	}
+
+	public void setPerna(double perna) {
+		this.perna = perna;
+	}
 	
 }
