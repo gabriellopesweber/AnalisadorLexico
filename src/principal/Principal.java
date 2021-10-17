8ab21dfa15fc7e7b77e6145b35afc7cbd442c5bc
Rafael Fagundes <rafaelfagundes64@gmail.com>
2021-10-17 16:10:30 -0300

    Lendo arquivo txt e percorrendo linhas

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,13 +1,39 @@
 package principal;
 
-import javax.swing.JOptionPane;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		String texto = JOptionPane.showInputDialog(null, "Digite aqui: ");
+		List<String> linhas = Files.readAllLines(Paths.get("teste.txt"), Charset.defaultCharset());
 		
-		System.out.println("texto: "+texto);
+		percorreTexto(linhas);
 	}
+	
+	public static void percorreTexto(List<String> linhas) {
+		boolean continua=true;
+		
+		while(continua) {
+			for (String linha : linhas) {
+				for (int i=0; i<linha.length(); i++) {
+				   char caracter = linha.charAt(i);
+				   
+				   if(Character.isLetter(caracter)) {
+					   System.out.println("letra: "+caracter);
+				   }
+					if(Character.isDigit(caracter)) {
+						   System.out.println("digito: "+caracter);
+				   }
+				  
+				}
+			}
+			continua=false;
+		}
+	}
+	
 }
