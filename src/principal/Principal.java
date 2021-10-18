cd3cc5155f8e04570300c50f3d1098f029954c4d
Gabriel Lopes Weber <gabriellw.grb@gmail.com>
2021-10-18 19:47:55 -0300

    Formatando o codigo.

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -9,65 +9,46 @@ import java.util.List;
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
-		
 		List<String> linhas = Files.readAllLines(Paths.get("teste.txt"), Charset.defaultCharset());
-		
 		percorreTexto(linhas);
 	}
-	
+
 	public static void percorreTexto(List<String> linhas) {
-		boolean continua=true;
-		
-		while(continua) {
+		boolean continua = true;
+
+		while (continua) {
 			for (String linha : linhas) {
-				for (int i=0; i<linha.length(); i++) {
-				   char caracter = linha.charAt(i);
-				   
-				   if(Character.isLetter(caracter)) {
-					   System.out.println("letra: "+caracter);
-				   }
-				   else if(Character.isDigit(caracter)) {
-					   System.out.println("digito: "+caracter);
-				   }
-				   else if(isOperador(caracter)) {
-					   System.out.println("operador: "+caracter);
-				   }
-				   else if(isAtribuicao(caracter)) {
-					   System.out.println("atribuicao: "+caracter);
-				   }
+				for (int i = 0; i < linha.length(); i++) {
+					char caracter = linha.charAt(i);
+
+					if (Character.isLetter(caracter)) {
+						System.out.println("letra: " + caracter);
+					} else if (Character.isDigit(caracter)) {
+						System.out.println("digito: " + caracter);
+					} else if (isOperador(caracter)) {
+						System.out.println("operador: " + caracter);
+					} else if (isAtribuicao(caracter)) {
+						System.out.println("atribuicao: " + caracter);
+					}
 				}
 			}
-			continua=false;
+			continua = false;
 		}
 	}
-	
+
 	public static boolean isAtribuicao(char caracter) {
 		if (caracter == "=".charAt(0)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public static boolean isOperador(char caracter) {
-		if(
-			caracter == "+".charAt(0) ||
-			caracter == "-".charAt(0) ||
-			caracter == "/".charAt(0) ||
-			caracter == "*".charAt(0)
-		) {
+		if (caracter == "+".charAt(0) || caracter == "-".charAt(0) || caracter == "/".charAt(0)
+				|| caracter == "*".charAt(0)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
