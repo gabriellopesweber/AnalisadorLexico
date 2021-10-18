959d156b79ecd40eaabb9d7736270e3fe7a991bc
Rafael Fagundes <rafaelfagundes64@gmail.com>
2021-10-18 20:51:47 -0300

    Quase pronto

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -18,34 +18,102 @@ public class Principal {
 
 		while (continua) {
 			for (String linha : linhas) {
-				for (int i = 0; i < linha.length(); i++) {
-					char caracter = linha.charAt(i);
-
-					if (Character.isLetter(caracter)) {
-						System.out.println("letra: " + caracter);
-					} else if (Character.isDigit(caracter)) {
-						System.out.println("digito: " + caracter);
-					} else if (isOperador(caracter)) {
-						System.out.println("operador: " + caracter);
-					} else if (isAtribuicao(caracter)) {
-						System.out.println("atribuicao: " + caracter);
-					}
+				
+				// pula linha vazia
+				if (linha.replaceAll("\\s","").length() == 0) {
+					continue;
 				}
+				
+				String[] palavras = linha.split(" ");
+				
+				for (String palavra : palavras) {
+					
+					palavra = palavra.replaceAll("\\s","");
+			
+//					System.out.println(" -- exibição: "+palavra);
+					
+
+					if (isOperadorLogico(palavra)) {
+						System.out.println("Operador Logico: " + palavra);
+					}
+					else if (isReservada(palavra)) {
+						System.out.println("Palavra Reservada: " + palavra);
+					}
+					else if (isOperadorAritmetico(palavra)) {
+						System.out.println("Operador Aritmetico: " + palavra);
+					}
+					else if (isAtribuicao(palavra)) {
+						System.out.println("Atribuicao: " + palavra);
+					}
+					else if(Character.isDigit(palavra.charAt(0))) {
+						System.out.println("Número: "+palavra);
+					}else if(Character.isLetter(palavra.charAt(0))) {
+						System.out.println("Identificador: "+palavra);
+					}else {
+						System.err.println("Palavra não encontrada: "+palavra);
+					}
+					
+					
+					
+				}
+				System.out.println("");
 			}
 			continua = false;
 		}
 	}
-
-	public static boolean isAtribuicao(char caracter) {
-		if (caracter == "=".charAt(0)) {
+	
+	// verifica se é uma palavra reservada
+	public static boolean isReservada(String palavra) {
+		
+		if(
+			palavra.equalsIgnoreCase("if") ||
+			palavra.equalsIgnoreCase("for") ||
+			palavra.equalsIgnoreCase("while") ||
+			palavra.equalsIgnoreCase(";") ||
+			palavra.equalsIgnoreCase("int") ||
+			palavra.equalsIgnoreCase("double") ||
+			palavra.equalsIgnoreCase("{") ||
+			palavra.equalsIgnoreCase("}") ||
+			palavra.equalsIgnoreCase("[") ||
+			palavra.equalsIgnoreCase("]") ||
+			palavra.equalsIgnoreCase("string") ||
+			palavra.equalsIgnoreCase("float") ||
+			palavra.equalsIgnoreCase("(") ||
+			palavra.equalsIgnoreCase(")") ||
+			palavra.equalsIgnoreCase("return") ||
+			palavra.equalsIgnoreCase("do") ||
+			palavra.equalsIgnoreCase("foreach")
+		) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isOperadorLogico(String palavra) {
+		if (
+				palavra.equalsIgnoreCase("<") ||
+				palavra.equalsIgnoreCase(">") ||
+				palavra.equalsIgnoreCase("=>") ||
+				palavra.equalsIgnoreCase("<=") ||
+				palavra.equalsIgnoreCase("==")
+			) {
 			return true;
 		}
 		return false;
 	}
 
-	public static boolean isOperador(char caracter) {
-		if (caracter == "+".charAt(0) || caracter == "-".charAt(0) || caracter == "/".charAt(0)
-				|| caracter == "*".charAt(0)) {
+	// verifica se é uma atribuição
+	public static boolean isAtribuicao(String palavra) {
+		if (palavra.equalsIgnoreCase("=")) {
+			return true;
+		}
+		return false;
+	}
+
+	// verifica se é um operador
+	public static boolean isOperadorAritmetico(String palavra) {
+		if (palavra.equalsIgnoreCase("+") || palavra.equalsIgnoreCase("-") || palavra.equalsIgnoreCase("/")
+				|| palavra.equalsIgnoreCase("*")) {
 			return true;
 		}
 		return false;
--- src/principal/Token.java
+++ src/principal/Token.java
@@ -1,43 +1,6 @@
 package principal;
 
 public class Token {
-	private double tokenInicial;
-	private double tokenFinal;
-
-	private String[] tipoDeAcao; // Valida/Atribui/Remove
-
-	private double perna;
-
-	public double getTokenInicial() {
-		return tokenInicial;
-	}
-
-	public void setTokenInicial(double tokenInicial) {
-		this.tokenInicial = tokenInicial;
-	}
-
-	public double getTokenFinal() {
-		return tokenFinal;
-	}
-
-	public void setTokenFinal(double tokenFinal) {
-		this.tokenFinal = tokenFinal;
-	}
-
-	public String[] getTipoDeAcao() {
-		return tipoDeAcao;
-	}
-
-	public void setTipoDeAcao(String[] tipoDeAcao) {
-		this.tipoDeAcao = tipoDeAcao;
-	}
-
-	public double getPerna() {
-		return perna;
-	}
-
-	public void setPerna(double perna) {
-		this.perna = perna;
-	}
-
+	
+	private String tipo = "identificador"
 }
