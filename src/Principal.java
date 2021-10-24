bdc3b1fa4b1f88372dd35e7d5e8503553ab8caa8
Rafael Fagundes <rafaelfagundes64@gmail.com>
2021-10-24 19:23:58 -0300

    falta so for e atribuicao

--- src/Principal.java
+++ src/Principal.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class Principal {
 
 	static Identificador a = new Identificador();
-
+	static String tipo_erro;
+	
 	public static void main(String[] args) throws IOException {
 		List<String> linhas = Files.readAllLines(Paths.get("teste.txt"), Charset.defaultCharset());
 		percorreTexto(linhas);
@@ -57,22 +58,40 @@ public class Principal {
 				cont++;
 
 				// VERIFICAR SINTAXE DA LINHA
-//				if (
-//						linha_com_token.get(0).getTipo().equalsIgnoreCase(a.PALAVRA_PRIMITIVA) && 
-//						linha_com_token.get(1).getTipo().equalsIgnoreCase(a.IDENTIFICADOR) && 
-//						linha_com_token.get(2).getTipo().equalsIgnoreCase(a.ATRIBUICAO) && 
-//						linha_com_token.get(3).getTipo().equalsIgnoreCase(a.PALAVRA_RESERVADA) && 
-//						linha_com_token.get(3).getTexto().equalsIgnoreCase(";")						
-//					) {
-//					System.out.println("declaração de variavel correta!");
-//					
-//				}else {
-//					System.err.println("declaração de variavel incorreta !"+linha_com_token.get(0).getLinha());
+				System.out.println(linhaToString(linha_com_token) + "\n");
+
+//				if (!isDeclaracao(linha_com_token) && !isAtribuicao(linha_com_token) && !isCondicao(linha_com_token)) {
+//					System.err.println("Erro: "+tipo_erro+" na linha " + linha_com_token.get(0).getLinha() + "\n");
 //				}
-				System.out.println(linhaToString(linha_com_token));
-				if (!isDeclaracao(linha_com_token) && !isAtribuicao(linha_com_token) && !isValidacao(linha_com_token)) {
-					System.err.println("Erro de sintaxe na linha " + linha_com_token.get(0).getLinha());
+				boolean achou_funcao = false;
+				
+				if(linha_com_token.get(0).getTipo().equalsIgnoreCase(a.PALAVRA_PRIMITIVA)){
+					if (!isDeclaracao(linha_com_token)) {
+						System.err.println("Erro: "+tipo_erro+" na linha " + linha_com_token.get(0).getLinha() + "\n");
+						continue;
+					}else {
+						achou_funcao = true;
+					}
 				}
+				
+				
+				
+				if(linha_com_token.get(0).getTipo().equalsIgnoreCase(a.IDENTIFICADOR) && !achou_funcao && !isAtribuicao(linha_com_token)) {
+					System.err.println("Erro: "+tipo_erro+" na linha " + linha_com_token.get(0).getLinha() + "\n");
+					continue;
+				}else {
+					achou_funcao = true;
+				}
+				
+
+				if(linha_com_token.get(0).getTipo().equalsIgnoreCase(a.PALAVRA_RESERVADA) && !achou_funcao && !isCondicao(linha_com_token)) {
+					System.err.println("Erro: "+tipo_erro+" na linha " + linha_com_token.get(0).getLinha() + "\n");
+					continue;
+				}else {
+					achou_funcao = true;
+				}
+				
+
 
 			}
 
@@ -81,17 +100,17 @@ public class Principal {
 	}
 
 	private static String linhaToString(ArrayList<Token> linha) {
-		String str = linha.get(0).getLinha() + " - ";
+		String str = "Linha: " + linha.get(0).getLinha() + "\n	";
 		for (Token elemento : linha) {
-			str += elemento.getTexto() + " <=[" + elemento.getTipo() + "] ";
+			str += elemento.getTexto() + " <=[" + elemento.getTipo() + "]\n	";
 		}
 		return str;
 	}
 
-	private static boolean isValidacao(ArrayList<Token> elemento) { // Esta funcao realiza a analise da sintaxe do IF,
-																	// onde pode ocorrer o cenario if ( a == b ) {
-																	// ou if ( a == b || b == a ) { entre outros.
-																	// AINDA NAO COMPLETA.
+	// Esta funcao realiza a analise da sintaxe do IF, onde pode ocorrer o cenario
+	// if ( a == b ) {
+	private static boolean isCondicao(ArrayList<Token> elemento) {
+
 		if (elemento.size() > 1) {
 			if (elemento.get(0).getTipo().equalsIgnoreCase(a.PALAVRA_RESERVADA)
 					&& (elemento.get(1).getTipo().equalsIgnoreCase(a.PALAVRA_RESERVADA)
@@ -102,11 +121,12 @@ public class Principal {
 							|| elemento.get(elemento.size() - 1).getTexto().equalsIgnoreCase("{"))) {
 				if (elemento.get(2).getTipo().equalsIgnoreCase(a.IDENTIFICADOR)
 						&& elemento.get(3).getTipo().equalsIgnoreCase(a.OPERADOR_LOGICO)
-						&& (!elemento.get(3).getTexto().equalsIgnoreCase("||")
-								|| !elemento.get(3).getTexto().equalsIgnoreCase("&&"))
-						&& elemento.get(4).getTipo().equalsIgnoreCase(a.IDENTIFICADOR)) {
+						&& (elemento.get(4).getTipo().equalsIgnoreCase(a.IDENTIFICADOR) ||
+								elemento.get(4).getTipo().equalsIgnoreCase(a.NUMERO) ||
+								elemento.get(4).getTipo().equalsIgnoreCase(a.STRING))) {
 					return true;
 				} else {
+					tipo_erro = "condicao invalida";
 					return false;
 				}
 
@@ -120,7 +140,7 @@ public class Principal {
 	private static boolean isAtribuicao(ArrayList<Token> elemento) { // Pensar num metodo mais composto, pois, caso
 																		// tenha a inserção de mais uma variavel, pode
 																		// gerar erro. Exemplo a = a + 2 + 2 + 2 + 2 ;
-		if (elemento.size() > 1) {
+		if (elemento.size() >= 4) {
 			if (elemento.get(0).getTipo().equalsIgnoreCase(a.IDENTIFICADOR)
 					&& elemento.get(1).getTipo().equalsIgnoreCase(a.ATRIBUICAO)
 					&& (elemento.get(2).getTipo().equalsIgnoreCase(a.NUMERO)
@@ -130,22 +150,31 @@ public class Principal {
 				return true;
 			}
 		}
+		tipo_erro = "atribuicao invalida";
 		return false;
 	}
 
 	private static boolean isDeclaracao(ArrayList<Token> elemento) {
-		if (elemento.size() > 1) {
-			if (elemento.get(0).getTipo().equalsIgnoreCase(a.PALAVRA_PRIMITIVA)
-					&& elemento.get(1).getTipo().equalsIgnoreCase(a.IDENTIFICADOR)
-					&& elemento.get(2).getTipo().equalsIgnoreCase(a.ATRIBUICAO)
-					&& (elemento.get(3).getTipo().equalsIgnoreCase(a.NUMERO)
-							|| elemento.get(3).getTipo().equalsIgnoreCase(a.STRING))
-					&& elemento.get(4).getTipo().equalsIgnoreCase(a.PALAVRA_RESERVADA)
-					&& elemento.get(4).getTexto().equalsIgnoreCase(";")) {
-				return true;
+		try {
+			if (elemento.size() >=5) {
+				if (elemento.get(0).getTipo().equalsIgnoreCase(a.PALAVRA_PRIMITIVA)
+						&& elemento.get(1).getTipo().equalsIgnoreCase(a.IDENTIFICADOR)
+						&& elemento.get(2).getTipo().equalsIgnoreCase(a.ATRIBUICAO)
+						&& (elemento.get(3).getTipo().equalsIgnoreCase(a.NUMERO)
+								|| elemento.get(3).getTipo().equalsIgnoreCase(a.STRING)
+							)
+						&& elemento.get(4).getTipo().equalsIgnoreCase(a.PALAVRA_RESERVADA)
+						&& elemento.get(4).getTexto().equalsIgnoreCase(";")
+					) {
+					return true;
+				}
 			}
+			tipo_erro = "declaracao invalida";
+			return false;
+		} catch (Exception e) {
+			tipo_erro = "declaracao invalida";
+			return false;
 		}
-		return false;
 	}
 
 	// verifica se é uma palavra reservada
